e78ed012c8df17262ccd28901bac6f9b1bd9b20b
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2024-01-16 23:12:22 +0000

    word embeddings: add xlm driver

--- word-embeddings/README.md
+++ word-embeddings/README.md
@@ -101,8 +101,9 @@ The following scripts exist:
 - **`embed_clip.py`:** Embeds using [CLIP](https://github.com/openai/CLIP)
 - **`embed_glove.py`:** Embeds using [GloVe](https://nlp.stanford.edu/projects/glove/). Requires the extra environment variable `GLOVE` to be set to a GloVe file (e.g. `glove.twitter.27B.200d.txt`).
 - **`embed_mbert.py`:** Embeds using [multilingual BERT](https://huggingface.co/bert-base-multilingual-uncased) - specifically `bert-base-multilingual-uncased`
+- **`embed_xlm.py`:** Embeds using [xlm-roberta-large](https://huggingface.co/xlm-roberta-large) - specifically
 
-If anyone knows of where the source code repository for this model is that isn't at huggingface.co (HuggingFace *sucks*), please let me know and I'll update these links.
+If anyone knows of where the source code repository for these models that isn't at huggingface.co (HuggingFace *sucks*), please let me know and I'll update these links.
 
 Embed all wordlists in a given directory with a given algorithm (e.g. CLIP in this example):
 
---
+++ word-embeddings/embed_xlm.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+import os
+import sys
+import io
+import json
+
+from transformers import AutoTokenizer, XLMRobertaModel
+import torch
+
+from lib.handle_open import handle_open
+
+
+### ENV VARS ################################################################
+if not "INPUT" in os.environ:
+	raise Exception("Error: INPUT environment variable not specified.")
+if not "OUTPUT" in os.environ:
+	raise Exception("Error: OUTPUT environment variable not specified.")
+
+INPUT=os.environ["INPUT"]
+OUTPUT=os.environ["OUTPUT"]
+
+if not os.path.exists(INPUT):
+	raise Exception(f"Error: INPUT filepath {INPUT} does not exist.")
+
+
+
+
+### FUNCTIONS ################################################################
+
+def flatten(xss):
+	"""
+	Ref https://stackoverflow.com/a/952952/1460422
+	"""
+	return [x for xs in xss for x in xs]
+
+
+### EMBEDDING ################################################################
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+tokenizer = AutoTokenizer.from_pretrained('xlm-roberta-large', device=device)
+model = XLMRobertaModel.from_pretrained('xlm-roberta-large', return_dict=True).to(device)
+
+
+
+handle_out = handle_open(OUTPUT, "w")
+with handle_open(INPUT, "r") as handle_in:
+	i=0
+	for line in handle_in:
+		line = line.strip()
+		if line == "":
+			continue
+		
+		try:
+			if type(line) is bytes:
+				line = line.decode("utf-8")
+		except UnicodeDecodeError as error:
+			sys.stderr.write("\n"+repr(error))
+			continue
+		
+		
+		try:
+			# Ref https://huggingface.co/transformers/v3.5.1/model_doc/bert.html#transformers.BertModel.forward
+			inputs = tokenizer(line, return_tensors="pt").to(device)
+			outputs = model(**inputs)
+			
+			last_hidden_states = outputs.last_hidden_state.flatten().tolist()
+		except Exception as error:
+			sys.stderr.write("\n"+repr(error))
+			continue		
+		
+		
+		handle_out.write((line + "\t" + "\t".join([str(value) for value in last_hidden_states]) + "\n").encode("utf-8"))
+		
+		if i % 100 == 0:
+			sys.stderr.write(f"Processed {i} words\r")
+		
+		i += 1
+
+handle_out.close()
